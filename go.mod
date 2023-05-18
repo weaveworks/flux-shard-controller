9dfb0c0ed92c45ff0782cce0c363da3898a9e1c4
Rana Tarek Hassan <rana.hassan@weave.works>
2023-05-18 19:23:34 +0300

    Add test for filtering deployments based on matching with shards in fluxshardset

--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module github.com/weaveworks/flux-shard-controller
 go 1.19
 
 require (
+	github.com/google/go-cmp v0.5.9
 	github.com/onsi/ginkgo/v2 v2.6.0
 	github.com/onsi/gomega v1.24.1
+	k8s.io/api v0.26.1
 	k8s.io/apimachinery v0.26.1
 	k8s.io/client-go v0.26.1
 	sigs.k8s.io/controller-runtime v0.14.4
+	sigs.k8s.io/yaml v1.3.0
 )
 
 require (
@@ -26,7 +29,6 @@ require (
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/gnostic v0.5.7-v3refs // indirect
-	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
 	github.com/google/uuid v1.1.2 // indirect
 	github.com/imdario/mergo v0.3.6 // indirect
@@ -58,7 +60,6 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.26.1 // indirect
 	k8s.io/apiextensions-apiserver v0.26.1 // indirect
 	k8s.io/component-base v0.26.1 // indirect
 	k8s.io/klog/v2 v2.80.1 // indirect
@@ -66,5 +67,4 @@ require (
 	k8s.io/utils v0.0.0-20221128185143-99ec85e7a448 // indirect
 	sigs.k8s.io/json v0.0.0-20220713155537-f223a00ba0e2 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
-	sigs.k8s.io/yaml v1.3.0 // indirect
 )
--- internal/deploys/deploy_test.go
+++ internal/deploys/deploy_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/weaveworks/flux-shard-controller/api/v1alpha1"
 	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/yaml"
@@ -26,6 +27,86 @@ func NewDeploymentFromDeployment(src appsv1.Deployment) *appsv1.Deployment {
 	src.Labels["app.kubernetes.io/managed-by"] = "flux-shard-controller"
 
 	return &src
+}
+
+// GetDeploymentsMatchingFluxShardSet returns a list of deployments that match the shards in fluxShardSet given
+func GetDeploymentsMatchingFluxShardSet(fluxShardSet *v1alpha1.FluxShardSet, allDeployments []*appsv1.Deployment) ([]*appsv1.Deployment, error) {
+	matchingDeployments := []*appsv1.Deployment{}
+
+	for _, d := range allDeployments {
+		if DeploymentMatchesShard(fluxShardSet, *d) {
+			matchingDeployments = append(matchingDeployments, d)
+		}
+	}
+
+	return matchingDeployments, nil
+}
+
+// // Given a fluxShardSet, return a list of names/labels to match with deployments
+// func ShardNamesFromFluxShardSets(fluxShardSet *v1alpha1.FluxShardSet) ([]string, error) {
+// 	shards := []string{}
+// 	for _, shard := range fluxShardSet.Spec.Shards {
+// 		shards = append(shards, shard.Name)
+// 	}
+// 	return shards, nil
+
+// }
+
+// DeploymentMatchesShard returns true if the deployment matches a shard in the fluxShardSet
+// matching is done by comparing the shards list and the deployment name
+func DeploymentMatchesShard(fluxShardSet *v1alpha1.FluxShardSet, deployment appsv1.Deployment) bool {
+	for _, shard := range fluxShardSet.Spec.Shards {
+		if shard.Name == deployment.Name {
+			return true
+		}
+	}
+	return false
+}
+
+func TestDeploymentMatchesShardfn(t *testing.T) {
+	fluxShardSet := &v1alpha1.FluxShardSet{}
+	fluxShardSet.Spec.Shards = append(fluxShardSet.Spec.Shards, v1alpha1.ShardSpec{Name: "shard1"})
+
+	deployment := appsv1.Deployment{}
+	// matching false when it should match
+	deployment.Name = "shard1"
+	if !DeploymentMatchesShard(fluxShardSet, deployment) {
+		t.Fatalf("failed to match shard1")
+	}
+
+	// Matching true when it shouldnt
+	deployment.Name = "shard2"
+	if DeploymentMatchesShard(fluxShardSet, deployment) {
+		t.Fatalf("matched shard2")
+	}
+}
+
+func TestMatchDeploymentsWithShards(t *testing.T) {
+	deployments := []*appsv1.Deployment{}
+	deployments = append(deployments, loadDeploymentFixture(t, "testdata/kustomize-controller.yaml"))
+	deployments = append(deployments, loadDeploymentFixture(t, "testdata/kustomize-controller-2.yaml"))
+
+	fluxShardSetFilename := "testdata/flux-shard-set.yaml"
+	b, err := os.ReadFile(fluxShardSetFilename)
+	if err != nil {
+		t.Fatalf("failed to read flux-shard-set: %s", err)
+	}
+
+	fluxShardSet := &v1alpha1.FluxShardSet{}
+	if err := yaml.Unmarshal(b, fluxShardSet); err != nil {
+		t.Fatalf("failed to unmarshal YAML fixture %s: %s", fluxShardSetFilename, err)
+	}
+
+	matchingDeps, err := GetDeploymentsMatchingFluxShardSet(fluxShardSet, deployments)
+	if err != nil {
+		t.Fatalf("failed to match deployments with shards: %s", err)
+	}
+
+	want := []*appsv1.Deployment{loadDeploymentFixture(t, "testdata/kustomize-controller.yaml"), loadDeploymentFixture(t, "testdata/kustomize-controller-2.yaml")}
+
+	if diff := cmp.Diff(want, matchingDeps); diff != "" {
+		t.Fatalf("failed to generate new deployment:\n%s", diff)
+	}
 }
 
 func TestNewDeploymentFromDeployment(t *testing.T) {
